c23dd7c714bb5b429e4e8ae20c2340a5c4817e33
Jeremy Keiper <jeremy@openmrs.org>
2014-03-24 20:15:38 +0000

    minor changes to improve compilation

--- api/src/main/java/org/openmrs/module/messagingphr/extension/html/MessagingPhrGutterExt.java
+++ api/src/main/java/org/openmrs/module/messagingphr/extension/html/MessagingPhrGutterExt.java
@@ -6,38 +6,38 @@ import org.openmrs.module.web.extension.LinkExt;
 
 public class MessagingPhrGutterExt extends LinkExt {
 
-    /**
-     * default constructor: set display order attribute
-     */
-    public MessagingPhrGutterExt() {
-        super();
-    }
+	/**
+	 * default constructor: set display order attribute
+	 */
+	public MessagingPhrGutterExt() {
+		super();
+	}
 
-    
+
 	@Override
-    public Extension.MEDIA_TYPE getMediaType(){
+	public Extension.MEDIA_TYPE getMediaType() {
 		return Extension.MEDIA_TYPE.html;
 	}
-	
 
-    @Override
-    public String getUrl() {
-        return "module/messagingphr/inbox.form?inTab=false";
-    }
-			
+
+	@Override
+	public String getUrl() {
+		return "module/messagingphr/inbox.form?inTab=false";
+	}
+
 	@Override
 	public String getRequiredPrivilege() {
-		return MessagingConstants.PRIV_VIEW_ALL_MESSAGES;
+		return MessagingConstants.PRIV_VIEW_MESSAGES;
 	}
 
 
-    /* (non-Jsdoc)
-     * @see org.openmrs.module.web.extension.LinkExt#getLabel()
-     */
-    @Override
-    public String getLabel() {
-        return "My Mail";
-    }
+	/* (non-Jsdoc)
+	 * @see org.openmrs.module.web.extension.LinkExt#getLabel()
+	 */
+	@Override
+	public String getLabel() {
+		return "My Mail";
+	}
 
 
 }
